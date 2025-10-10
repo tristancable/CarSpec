ded11e6b3a7e7a9ad16466749d94d9fc1230a6b3
tristancable <tcable@student.neumont.edu>
2025-10-10 15:14:24 -0600

    initialization of CarSpec for .NET MAUI Blazor Hybrid created

---
+++ CarSpec/CarSpec/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CarSpec
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
--- CarSpec/CarSpec/CarSpec.Shared/Services/IFormFactor.cs
+++
@@ -1,8 +0,0 @@
-namespace CarSpec.Shared.Services
-{
-    public interface IFormFactor
-    {
-        public string GetFormFactor();
-        public string GetPlatform();
-    }
-}
